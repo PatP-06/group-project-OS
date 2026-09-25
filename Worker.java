22334d662ee05a8e77c82993b551a87be0112fa8
Meow-nah-ngo <thitaree1811@gmail.com>
2026-09-25 12:11:16 +0700

    add null exception in worker
    wait to fixing

--- Worker.java
+++ Worker.java
@@ -12,28 +12,51 @@ public class Worker implements Runnable {
     @Override
     public void run() {
         while(true) {
+            Request request = null; // Null exception in case bugged worker doesn't know their status as they carried the request or not ; what ifs error[catch] in error[null]
+
             try {
                 Request request = MessageQueue.getRequest();
                 ServerLogger.log("Worker-" + workerId, "RECEIVED", request.getCommand() + " " + request.getTicketId() + " from " + request.getClientId());
 
-                if (request.getCommand().equals("RESERVE")) {
-                    boolean success = manager.reserve(request.getTicketId(), request.getClientId(), workerId);
+                if ("RESERVE".equals(request.getCommand())){ // Null exception in case bugged string == null (request.getCommand().equals("RESERVE")[old code] ; null = 'reserve' #null)
+
+                    boolean success = manager.reserve(
+                        request.getTicketId(),
+                        request.getClientId(),
+                        workerId
+                    );
                     if (success) {
-                        request.getResponse().complete(request.getTicketId());
-                    } else {
-                        request.getResponse().complete(request.getTicketId());
+                        request.getResponse()
+                            .complete(request.getTicketId());
                     }
-                } else if (request.getCommand().equals("CANCEL")) {
-                    boolean success = cancelManager.cancelTicket(request.getTicketId(), request.getClientId(), workerId);
+                    else {
+                        request.getResponse()
+                            .complete("Reservation failed");
+                    }
+                else if ("CANCEL".equals(request.getCommand())) {
+
+                    boolean success = cancelManager.cancelTicket(
+                        request.getTicketId(),
+                        request.getClientId(),
+                        workerId
+                    );
                     if (success) {
-                        request.getResponse().complete(request.getTicketId());
+                        request.getResponse()
+                            .complete(request.getTicketId());
                     } else {
-                        request.getResponse().complete("FAILED: You are not the owner");
+                        request.getResponse()
+                            .complete("You are not the owner");
                     }
+                }
+                else {
+                    request.getResponse().complete("Unknown");
                 }
 
             } catch(Exception e) {
                 e.printStackTrace();
+                if (request != null) {
+                    request.getResponse()
+                        .complete("FAILED: Internal server error");
             }
         }
     }
